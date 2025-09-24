d1673748de60a0f2ed993004f407a6237b4e05bc
Ankush Kumar Jha <jha.akj05@gmail.com>
2025-09-24 11:57:46 +0530

    Linked list.cpp

---
+++ Linked list.cpp
@@ -0,0 +1,27 @@
+//Ankush Jha
+//PRN:24070123016
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+int main(){
+    Node* head = new Node(10);
+    cout<<head->val<<" "<<head->next;
+    return 0;
+
+}
+
+/* OUTPUT:
+10 0
+*/
